e2d0e720d69991c35b3c44e60010a39f8d902059
Twan <twanvandenbor@hotmail.com>
2021-09-14 16:11:43 +0200

    US-001 Add DamTile component

---
+++ src/components/DamTile.vue
@@ -0,0 +1,41 @@
+<template>
+	<div 
+		class="dam-tile-root"
+		:class="props.isWhite ? 'white-dam-tile' : 'black-dam-tile'"
+		:style="{
+			minWidth: `${66 / props.maximumDamTilesPerRow}vh`,
+			minHeight: `${66 / props.maximumDamTilesPerRow}vh`,
+        }"
+	>
+	</div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  name: 'aboutTheBakeryView',
+  props: {
+	isWhite: { type: Boolean, default: false },
+	maximumDamTilesPerRow: { type: Number, default: 8 }
+  },
+  setup(props) {
+    const data = reactive({});
+
+    return {
+      data,
+      props
+    };
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.white-dam-tile {
+	background-color: white;
+}
+.black-dam-tile {
+	background-color: black;
+}
+</style>
