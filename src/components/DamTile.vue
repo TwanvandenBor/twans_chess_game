f17a6aab6501b09a4abe8b4c66893e20ffa0d2e9
Twan <twanvandenbor@hotmail.com>
2021-09-14 17:49:26 +0200

    US-001 Add damstone drawing section to DamTile

--- src/components/DamTile.vue
+++ src/components/DamTile.vue
@@ -7,18 +7,25 @@
 			minHeight: `${66 / props.maximumDamTilesPerRow}vh`,
         }"
 	>
+		<section 
+			class="dam-stone-drawing"
+			:class="props.damStone ? (damStone?.isColorWhite ? 'white-dam-stone' : 'black-dam-stone') : null"
+		>
+		</section>
 	</div>
 </template>
 
 
 <script lang="ts">
 import { defineComponent, reactive } from "vue";
+import { DamStone } from "@/model/DamStone";
 
 export default defineComponent({
   name: 'aboutTheBakeryView',
   props: {
 	isWhite: { type: Boolean, default: false },
-	maximumDamTilesPerRow: { type: Number, default: 8 }
+	maximumDamTilesPerRow: { type: Number, default: 8 },
+	damStone: { type: Object as () => DamStone }
   },
   setup(props) {
     const data = reactive({});
@@ -32,10 +39,33 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+.dam-tile-root {
+	position: relative;
+}
+
 .white-dam-tile {
 	background-color: white;
 }
 .black-dam-tile {
-	background-color: black;
+	background-color: #763D1B;
+}
+
+
+.dam-stone-drawing {
+	min-width: 66%;
+	min-height: 66%;
+	max-width: 66%;
+	max-height: 66%;
+	border-radius: 66%;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+.white-dam-stone {
+	background-color: white;
+}
+.black-dam-stone {
+	background-color: black;	
 }
 </style>
