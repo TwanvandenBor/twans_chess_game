5412609deb6c7fc746375ec10ddd63f5b318b0ea
Twan <twanvandenbor@hotmail.com>
2021-09-14 17:49:00 +0200

    US-001 Setup basic homepage styling

---
+++ src/views/home/Homepage.vue
@@ -0,0 +1,52 @@
+<template>
+	<div id="homepage-root">
+    <section id="chess-image-section">
+      <img 
+        id="chess-game-image"
+        src="@/assets/images/chess1.jpg" 
+        :alt="$t('chess_image_alt_description')"
+      >
+    </section>
+
+    <dam-board></dam-board>
+
+	</div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import DamBoard from "@/components/DamBoard.vue";
+
+export default defineComponent({
+  name: 'homepage',
+  components: { 
+    DamBoard
+  },
+  props: {},
+  setup() {
+    const data = reactive({});
+
+    return {
+      data
+    };
+  }
+});
+</script>
+
+<style scoped lang="scss">
+#homepage-root {
+  position: relative;
+}
+
+#chess-image-section {
+  position: fixed;
+  #chess-game-image {
+    object-fit: cover;
+    min-width: 100vw;
+    min-height: 100vh;
+    max-width: 100vw;
+    max-height: 100vh;
+  }
+}
+</style>
---
+++ src/views/layout/DefaultLayout.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="main-view-wrapper">
+		<router-view></router-view>
+	</div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  name: 'defaultLayout',
+  props: {},
+  setup() {
+    const data = reactive({});
+
+    return {
+      data
+    };
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.main-view-wrapper {
+	width: 100vw;
+	min-width: 100vw;
+	height: 100vh;
+	min-height: 100vh;
+	max-height: 100vh;
+	margin: 0;
+}
+</style>
