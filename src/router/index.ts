3049c591cab82448dea91a9769bc9218c42dc030
Twan <twanvandenbor@hotmail.com>
2021-09-14 11:32:29 +0200

    US-001 Delete default vue 3 project template code and setup basic route and scss

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,34 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/",
+    name: "defaultLayout",
+    component: () => import("@/views/layout/DefaultLayout.vue"),
+    redirect: "/homepage",
+    meta: {
+      requiresAuth: false,
+    },
+    children: [
+      {
+        path: "homepage",
+        name: "homepage",
+        component: () => import("@/views/home/Homepage.vue"),
+      },
+    ]
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if(savedPosition){
+      return savedPosition;
+    } else {
+      return { top: 0 }
+    }
+  },
+});
 
-export default router
+export default router;
